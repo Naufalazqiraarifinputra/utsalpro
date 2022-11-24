d3e27469028b62b0b3bcda1d2c291bb918d16773
Naufalazqiraarifinputra <118955065+Naufalazqiraarifinputra@users.noreply.github.com>
2022-11-24 11:45:25 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args) {
+        String salam = "Assalamualaikum";
+        String nama = "Naufal azqira arifin putra";
+        int lahir = 2001;
+        
+        System.out.println(salam);
+        System.out.println("Nama Saya "+nama);
+        System.out.println("Saya Lahir di Tahun : "+lahir);
+    }
+}
